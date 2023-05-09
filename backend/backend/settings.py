c92082e3dd5e045a2f2ccc3acd0e3f293a7abcc5
Lauri Leskinen <laurileskinen@icloud.com>
2023-05-09 15:01:23 +0300

    Test push

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,11 +70,6 @@ DATABASES = {
     }
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 16
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
