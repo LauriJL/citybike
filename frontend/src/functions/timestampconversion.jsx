f8458bbbad4f87b35db3804dfed432839a603ebd
Lauri Leskinen <laurileskinen@icloud.com>
2023-05-11 16:40:36 +0300

    Fixed timestamp conversion function.

--- frontend/src/functions/timestampconversion.jsx
+++ frontend/src/functions/timestampconversion.jsx
@@ -1,6 +1,14 @@
 const TimestampConversion = (timestamp) => {
-  let date = new Date(timestamp).toLocaleString();
-  return date;
+  let raw_string = JSON.stringify(timestamp);
+  let dd = raw_string.slice(9, 11);
+  let mm = raw_string.slice(6, 8);
+  let yy = raw_string.slice(1, 5);
+  let hh = raw_string.slice(12, 14);
+  let MM = raw_string.slice(15, 17);
+  let ss = raw_string.slice(18, 20);
+  let new_date =
+    hh + ":" + MM + ":" + ss + " (" + dd + "." + mm + "." + yy + ")";
+  return new_date;
 };
 
 export default TimestampConversion;
